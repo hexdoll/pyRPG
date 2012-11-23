1f5c38de538448f812c20191ea5287ce0c54360b
Gemma Peter <hexdoll@gmail.com>
2012-11-23 06:01:37 +0000

    add name parameter to scene

--- RPG/Scene.py
+++ RPG/Scene.py
@@ -2,15 +2,21 @@ import pygame
 
 class Scene:
     IMAGE_DIR = 'data/scenes/'
+    #background
+    #foreground
+    #collision
 
-    def __init__(self):
-        self.name = 'test'
+    def __init__(self, name='test'):
+        self.name = name
 
-        self.background = pygame.image.load(self.IMAGE_DIR+'background.png')
+        self.background = pygame.image.load(self.IMAGE_DIR + self.name + '/background.png')
         self.background = self.background.convert()
 
-        self.foreground = pygame.image.load(self.IMAGE_DIR+"foreground.png")
+        self.foreground = pygame.image.load(self.IMAGE_DIR + self.name + '/foreground.png')
         self.foreground = self.foreground.convert_alpha()
+
+        self.collision = pygame.image.load(self.IMAGE_DIR + self.name + '/collision.png')
+        self.collision = pygame.mask.from_surface(self.collision)
 
     def renderBackground(self, screen):
         screen.blit(self.background, (0,0))
